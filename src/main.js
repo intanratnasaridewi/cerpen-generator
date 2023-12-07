7241ad4f6d5fbcb7c74c6980222d99bbdd754c30
sekarwandani <sekarwandani761@gmail.com>
2023-12-07 22:45:08 +0700

    push

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+/**
+ * Create APP
+ */
+const app = createApp(App)
+
+app.mount('#app')
